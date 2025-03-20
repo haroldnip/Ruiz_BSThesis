Revised to cater proper stop spacing and corrected truck occupancy calculation

Source code (periodic, revised)/main_sim.py
@@ -48,47 +48,48 @@ class IntegratedSimulator:
         np.random.shuffle(self.vehicle_simulator.vehicles)
         np.random.shuffle(self.pedestrian_simulator.passengers)
         
-        for vehicle in self.vehicle_simulator.vehicles:
-            if vehicle.current_row == 1: # We  dont have to check for speed because vehicles cannot straddle if their v = 0
-                vehicle.finish_lane_change()
-                self.inform_driver_of_destination(vehicle)
+        if len(self.vehicle_simulator.vehicles) > 0:
+            for vehicle in self.vehicle_simulator.vehicles:
+                if vehicle.current_row == 1: # We  dont have to check for speed because vehicles cannot straddle if their v = 0
+                    vehicle.finish_lane_change()
+                    self.inform_driver_of_destination(vehicle)
 
-            elif np.random.rand() < vehicle.lane_changing_prob:
-                vehicle.accelerate()
-                vehicle.lane_changing()
-                vehicle.decelerate()
-                if vehicle.vehicle_type == "jeep":
-                    vehicle.unloading(timestep)
-                    vehicle.loading(timestep)
-                vehicle.random_slowdown()
-                vehicle.move() #Move the vehicle
-                self.inform_driver_of_destination(vehicle)
-        
-            else:
-                vehicle.accelerate() 
-                vehicle.decelerate() 
-                if vehicle.vehicle_type == "jeep":
-                    vehicle.unloading(timestep)
-                    vehicle.loading(timestep)
-                vehicle.random_slowdown()
-                vehicle.move()
-                self.inform_driver_of_destination(vehicle)
-            for passenger in vehicle.passengers_within_vehicle:
-                passenger.determine_if_overshot_destination(vehicle)
-            vehicle.temporal_speeds.append(vehicle.speed)
-            self.pedestrian_simulator.update_occupancy(timestep)
-            self.vehicle_simulator.update_occupancy(timestep)
-            #print(f"Passenger destinations of {vehicle.vehicle_type} {vehicle.vehicle_id} are {vehicle.destination_stops}")
-            # Count vehicles passing through the monitored position (e.g., position 99)
-            if timestep > transient_time:
-                self.counter.count_vehicle(vehicle)
-                self.counter.count_passenger(vehicle)
-                self.counter.count_jeeps(vehicle)
-                self.counter.count_trucks(vehicle)
-
-        for passenger_list in self.pedestrian_simulator.sidewalk.passengers:
-            for passenger in passenger_list:
+                elif np.random.rand() < vehicle.lane_changing_prob:
+                    vehicle.accelerate()
+                    vehicle.lane_changing()
+                    vehicle.decelerate()
+                    if vehicle.vehicle_type == "jeep":
+                        vehicle.unloading(timestep)
+                        vehicle.loading(timestep)
+                    vehicle.random_slowdown()
+                    vehicle.move() #Move the vehicle
+                    self.inform_driver_of_destination(vehicle)
+            
+                else:
+                    vehicle.accelerate() 
+                    vehicle.decelerate() 
+                    if vehicle.vehicle_type == "jeep":
+                        vehicle.unloading(timestep)
+                        vehicle.loading(timestep)
+                    vehicle.random_slowdown()
+                    vehicle.move()
+                    self.inform_driver_of_destination(vehicle)
+                for passenger in vehicle.passengers_within_vehicle:
+                    passenger.determine_if_overshot_destination(vehicle)
+                vehicle.temporal_speeds.append(vehicle.speed)
                 self.pedestrian_simulator.update_occupancy(timestep)
+                self.vehicle_simulator.update_occupancy(timestep)
+                #print(f"Passenger destinations of {vehicle.vehicle_type} {vehicle.vehicle_id} are {vehicle.destination_stops}")
+                # Count vehicles passing through the monitored position (e.g., position 99)
+                if timestep > transient_time:
+                    self.counter.count_vehicle(vehicle)
+                    self.counter.count_passenger(vehicle)
+                    self.counter.count_jeeps(vehicle)
+                    self.counter.count_trucks(vehicle)
+
+        # for passenger_list in self.pedestrian_simulator.sidewalk.passengers:
+        #     for passenger in passenger_list:
+        #         self.pedestrian_simulator.update_occupancy(timestep)
 
                 #Collect individual speeds of vehicles per timestep (To compute the spatial mean speed per timestep)
         if timestep >= transient_time:
@@ -100,6 +101,12 @@ class IntegratedSimulator:
 
             self.vehicle_simulator.occupancy_history.append((timestep, self.vehicle_simulator.road.occupancy.copy()))
             self.pedestrian_simulator.sidewalk_occupancy_history.append((timestep, self.pedestrian_simulator.sidewalk.occupancy.copy()))
+
+        for stop in self.pedestrian_simulator.sidewalk.stops:
+            if stop:
+                stop_interest = stop[0]
+                #print(f"For Stop at {stop_interest.position}, the loading list is {stop_interest.loading_list} with {len(stop_interest.loading_list)} elements.")
+                pass
     #Compute riding times once, not everytime step. Just compute it at the end.
 
     def inform_driver_of_destination(self, vehicle):
@@ -150,10 +157,10 @@ class IntegratedSimulator:
         while timestep < max_timesteps:
             # Update both simulators at each timestep
             if timestep < transient_time:
+                #print(f"Implementing rules for timestep {timestep}")
                 self.current_time = timestep
                 self.pedestrian_simulator.current_time = timestep
                 self.vehicle_simulator.current_time = timestep
-                #print(f"Implementing rules for timestep {timestep}")
                 
                 self.pedestrian_simulator.generate_passengers(self.vehicle_simulator, timestep)
                 self.populate_the_road(jeep_lane_change_prob, truck_lane_change_prob, self.pedestrian_simulator.sidewalk, timestep, transient_time, stop_to_stop_distance, safe_stopping_speed, safe_deceleration, jeepney_allowed_rows, truck_allowed_rows)  # Update vehicles
@@ -162,7 +169,7 @@ class IntegratedSimulator:
 
             # Collect data after transient period
             else: #timestep >= transient_time:
-                print(f"Implementing rules for timestep {timestep}")
+                #print(f"Implementing rules for timestep {timestep}")
                 self.current_time = timestep
                 self.pedestrian_simulator.current_time = timestep
                 self.vehicle_simulator.current_time = timestep
@@ -187,7 +194,7 @@ class IntegratedSimulator:
                 self.truck_throughput = self.counter.calculate_truck_throughput()
                 self.actual_density = (self.vehicle_simulator.spawned_vehicles_occupancy) / (self.vehicle_simulator.road.length * self.vehicle_simulator.road.width)
                 self.actual_truck_fraction = self.vehicle_simulator.spawned_trucks / self.vehicle_simulator.spawned_vehicles
-                self.actual_truck_occupancy_fraction = (self.vehicle_simulator.spawned_trucks*14)/ (self.vehicle_simulator.road.length * self.vehicle_simulator.road.width)
+                self.actual_truck_occupancy_fraction = (self.vehicle_simulator.spawned_trucks*14)/ ((self.vehicle_simulator.spawned_trucks*14)+(self.vehicle_simulator.spawned_jeeps*6))
                 vehicle_spatial_mean_speed = np.mean(self.vehicle_spatial_speeds)
                 jeep_spatial_mean_speed = np.mean(self.jeep_spatial_speeds)
                 truck_spatial_mean_speed = np.mean(self.truck_spatial_speeds)
@@ -239,6 +246,9 @@ class IntegratedSimulator:
             # if (passenger.jeep_boarding_time > transient_time and passenger.arrived_at_destination_time > transient_time):
             riding_time = passenger.riding_time
             waiting_time = passenger.waiting_time
+            spawning_time = passenger.sidewalk_entry_time
+            boarding_time = passenger.jeep_boarding_time
+            alighting_time = passenger.arrived_at_destination_time
             if passenger in self.pedestrian_simulator.waiting_passengers:
                 status = "Waiting"
             elif passenger in self.pedestrian_simulator.in_transit_passengers:
@@ -252,7 +262,10 @@ class IntegratedSimulator:
                 "Passenger ID": passenger.passenger_id,
                 "Riding Time": riding_time,
                 "Waiting Time": waiting_time,
-                "Riding Status":status})
+                "Riding Status":status,
+                "Spawning Time": spawning_time,
+                "Boarding Time": boarding_time,
+                "Alighting Time": alighting_time})
                 #"Passenger Travel Speed":passenger_travel_speed})
 
         #Collect vehicle data

Source code (periodic, revised)/passenger_sim.py
@@ -35,9 +35,13 @@ class Passenger_Simulator:
         # Reset the sidewalk occupancy matrix to zeros
         self.sidewalk.occupancy.fill(0)                 
         for x_position in range(self.sidewalk.length):
-            number_of_passengers_present = len(self.sidewalk.stops[x_position][0].loading_list)
-            # print(f"The number of passengers present at position {x_position} is {number_of_passengers_present}.")
-            self.sidewalk.occupancy[x_position] = number_of_passengers_present        # Count passengers for each cell
+            if self.sidewalk.stops[x_position]: 
+                number_of_passengers_present = len(self.sidewalk.stops[x_position][0].loading_list)
+                # print(f"The number of passengers present at position {x_position} is {number_of_passengers_present}.")
+            else:
+                number_of_passengers_present = 0
+            
+            self.sidewalk.occupancy[x_position] = number_of_passengers_present # Count passenger each cell
         # print(f"Hence, sidewalk occupancy is {self.sidewalk.occupancy}.")
         #self.sidewalk_occupancy_history.append(self.sidewalk.occupancy.copy())
 
@@ -61,12 +65,12 @@ class Passenger_Simulator:
             #print(f"The stops at position {position} is {self.sidewalk.boarding_stops[position]}")
             if len(self.sidewalk.stops[position]) != 0: #We spawn passengers on sidewalk cells with designated stops only (Here, we check for the presence of a stop)
                 
-                if self.sidewalk.frozen_cells[position]:# **Skip frozen cells**
-                    continue
+                # if self.sidewalk.frozen_cells[position]:# **Skip frozen cells**
+                #     print(f"Sidewalk position {position} skipped, frozen.")
+                #     continue
                 
                 if np.random.random() < self.passenger_arrival_rate:
                     if self.sidewalk.occupancy[position, 0] < self.sidewalk.max_passengers_per_cell:
-                        distance_within_sight_param = 4
                         destination = None 
                         set_of_stops = self.sidewalk.stops
                         
@@ -77,7 +81,7 @@ class Passenger_Simulator:
 
                         # print(f"The destination position for the passenger initiated at position {position} is {position}")
                         new_passenger = Passenger(current_time_pass, self.sidewalk, 
-                        self.road_designation, position, distance_within_sight_param, 
+                        self.road_designation, position, 
                         destination, vehicle_simulator, self)
                         # print(f"Passenger {new_passenger.passenger_id}'s destinations at {new_passenger.destination_stop}")
                         self.passengers.append(new_passenger)
@@ -88,7 +92,7 @@ class Passenger_Simulator:
                         # print(f"Passenger {new_passenger.passenger_id} is added to Stop at {self.sidewalk.stops[position][0].position}. The loading list is {self.sidewalk.stops[position][0].loading_list}.")
                         self.waiting_passengers.append(new_passenger)
                         self.update_occupancy(self.current_time)
-                        #print(f"Passenger added at position {col}.")
+                        #print(f"Passenger added at position {position}.")
                         #print(f"Passenger {new_passenger.passenger_id} was added to position {position}, where the destination is at {destination_position}.")
         # print(f"Time {self.current_time}: {len(self.passengers)} passengers generated.")
 

Source code (periodic, revised)/vehicle.py
@@ -56,6 +56,8 @@ class Vehicle:
         self.previous_speed = None
         self.previous_front_bumper_position = None
         self.passengers_on_board = {} #Dictionary for unloading purposes
+        # self.active_loading_list = []
+        # self.queue_loading_list = []
 
     def accelerate(self):
         """Increases vehicle's speed by 1 cell, up to the maximum speed"""
@@ -99,7 +101,7 @@ class Vehicle:
     def begin_straddling(self, direction):
         """This method executes the actual action of changing lanes (in particular, this methods let the vehicle begin straddling)
             This method assumes that the occupancy on the lane of interest was already checked."""
-        self.speed = self.gap_distance(self.current_row)
+        self.speed = max(0, self.gap_distance(self.current_row)-1)
         
         if self.speed == 0:
             self.accelerate()
@@ -415,16 +417,17 @@ class Vehicle:
         self.matching_stop_found = False  
         # print(f"Matching_stops_on_sidewalk: {self.vehicle_type} {self.vehicle_id}'s adjacent stops are {self.stop_list_adjacent}")
         
-        for stop in self.stop_list_adjacent:  # Iterate over adjacent stops
-            stop_list = self.sidewalk.stops[stop.position]  # This is a list of Stop objects
-            stop_concerned = stop_list[0] if stop_list else None  # Get the first Stop if available
+        for stop in self.stop_list_adjacent:  # Iterate over adjacent stops\
+            if stop:
+                stop_list = self.sidewalk.stops[stop.position]  # This is a list of Stop objects
+                stop_concerned = stop_list[0] if stop_list else None  # Get the first Stop if available
 
-            if stop_concerned and stop.unloading_dictionary:  # Check if stop exists and has passengers
-                for passenger_id in stop.unloading_dictionary.keys():
-                    if passenger_id in self.passengers_on_board:  # Match with passengers inside vehicle
-                        self.matching_stop_found = True
-                        # print(f"{self.vehicle_type} {self.vehicle_id} found a matching stop at {stop.position} for passenger {passenger_id}")
-                        return  # Exit early if match found
+                if stop_concerned and stop.unloading_dictionary:  # Check if stop exists and has passengers
+                    for passenger_id in stop.unloading_dictionary.keys():
+                        if passenger_id in self.passengers_on_board:  # Match with passengers inside vehicle
+                            self.matching_stop_found = True
+                            # print(f"{self.vehicle_type} {self.vehicle_id} found a matching stop at {stop.position} for passenger {passenger_id}")
+                            return  # Exit early if match found
 
         return  # No match found
 
@@ -470,18 +473,19 @@ class Vehicle:
                     if self.passengers_served > 0:
                         return 
                 for stop in self.stop_list_adjacent:
-                    # print(f"The unloading list of {stop} is {stop.unloading_list}")
+                    if stop:
+                        # print(f"The unloading list of {stop} is {stop.unloading_list}")
                     #self.update_pool_of_occupied_positions()
                     #(f"Passenger {passenger.passenger_id} is checking the stop {stop.position} and its unloading list {stop.unloading_list}")
-                    if passenger in stop.unloading_list and passenger.let_me_out: #Anticipates that chance of evenly-spaced stops
-                        # print(f"Passenger {passenger.passenger_id}'s destination is adjacent to the {self.vehicle_type} {self.vehicle_id} and the passenger is in the unloading list.")
-                        self.release_passenger(passenger, current_time)             
-                        self.passengers_served += 1
-                        # Track unloading from the far lane
-                        if self.current_row == 2:
-                            self.far_lane_unloadings += 1
-                        if self.passengers_served > 0:
-                            return    
+                        if passenger in stop.unloading_list and passenger.let_me_out: #Anticipates that chance of evenly-spaced stops
+                            # print(f"Passenger {passenger.passenger_id}'s destination is adjacent to the {self.vehicle_type} {self.vehicle_id} and the passenger is in the unloading list.")
+                            self.release_passenger(passenger, current_time)             
+                            self.passengers_served += 1
+                            # Track unloading from the far lane
+                            if self.current_row == 2:
+                                self.far_lane_unloadings += 1
+                            if self.passengers_served > 0:
+                                return    
  
         return
 
@@ -550,11 +554,13 @@ class Vehicle:
                 index = start_index
                 road_length = self.road_designation.length
                 # Handle periodic boundary conditions
-                road_positions = [(i % road_length) for i in range(start_index, end_index)]
+                #road_positions = [(i % road_length) for i in range(start_index, end_index)]
 
                 # Freeze sidewalk cells during loading
                 if self.speed == 0:
-                    self.sidewalk.freeze_cells_for_loading(road_positions)
+                    
+                    pass
+                    #self.sidewalk.freeze_cells_for_loading(road_positions)
 
                 # print(f"{self.vehicle_type} {self.vehicle_id} detected passengers starting from {start_index} to {end_index}.")
                 loaded_passengers = 0
@@ -563,7 +569,12 @@ class Vehicle:
                     #print(f"{self.vehicle_type} {self.vehicle_id} is currently checking at {index}.")
                     road_cell = index % road_length
                     sidewalk_cell = self.get_adjacent_sidewalk_cell(road_cell)
-                    passenger_list = self.sidewalk.stops[road_cell][0].loading_list
+                    #passenger_list = self.sidewalk.stops[road_cell][0].loading_list
+                    if self.sidewalk.stops[road_cell]:  # Ensure the list is not empty
+                        passenger_list = self.sidewalk.stops[road_cell][0].loading_list
+                    else:
+                        passenger_list = []  # No passengers if no stop exists
+
 
                     if sidewalk_cell == 0 or len(passenger_list) == 0:
                         index += 1
@@ -593,12 +604,15 @@ class Vehicle:
                         break
                     index += 1
                 # Unfreeze sidewalk cells once loading is complete
-                if len(self.last_loading_position)>0 and all(self.sidewalk.occupancy[position] == 0 for position in self.last_loading_position):
-                    # print(f"{self.vehicle_type} {self.vehicle_id} last loading position is {self.last_loading_position}.")
-                    rolled_sidewalk_occupancy = np.roll(self.sidewalk.occupancy, -self.last_loading_position[0])
-                    if self.speed > 0 or (np.sum(rolled_sidewalk_occupancy[0:self.length]==0)):
-                        self.sidewalk.unfreeze_cells(self.last_loading_position)
-                # print(f"Loaded {loaded_passengers} passengers. Current passengers: {len(self.passengers_within_vehicle)} ")
+                # print(f"Unfreezing check for {self.vehicle_type} {self.vehicle_id}")
+                # if len(self.last_loading_position)>0 and (all(self.sidewalk.occupancy[position] == 0 for position in self.last_loading_position) or self.capacity == len(self.passengers_within_vehicle)):
+                #     print(f"{self.vehicle_type} {self.vehicle_id} last loading position is {self.last_loading_position}.")
+                #     print(f"Sidewalk occupancy at those positions: {[self.sidewalk.occupancy[pos] for pos in self.last_loading_position]}")
+                #     rolled_sidewalk_occupancy = np.roll(self.sidewalk.occupancy, -self.last_loading_position[0])
+                #     print(f"Vehicle speed: {self.speed}, Occupied seats: {len(self.passengers_within_vehicle)}/{self.capacity}")
+                #     if self.speed > 0 or (np.sum(rolled_sidewalk_occupancy[0:self.length]==0) or self.capacity == len(self.passengers_within_vehicle)):
+                #         self.sidewalk.unfreeze_cells(self.last_loading_position)
+                #print(f"Loaded {loaded_passengers} passengers. Current passengers: {len(self.passengers_within_vehicle)} ")
         return
 
     def random_slowdown(self):